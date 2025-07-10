fdc1239314c3489c09a4753c155519c0e368775d
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-07-10 19:39:30 +0300

    configuration

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import FLask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = FLask(__name__)
 
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ArtSiteGallery.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
