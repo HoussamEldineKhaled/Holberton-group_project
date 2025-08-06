6a3e30540f9fe01c8f4cca1b4249c0993344b4d7
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-08-06 16:56:02 +0300

    deleted frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 from flask_cors import CORS
 
@@ -15,13 +15,31 @@ def get_db_connection():
 def get_tables():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Account;")
+    cursor.execute("SELECT * FROM Likes;")
     tables = cursor.fetchall()
     table_names = [table['User_Name'] for table in tables]
     
     conn.close()
     
     return jsonify(table_names)
+
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Account(Id, User_name, Email, Password, User_Role, Instagram, GitHub, Twitter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+    conn.commit()
+    conn.close()
+    return "User created account successfully", 200
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
