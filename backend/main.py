99e302413ca44da4187800788b3ccb63d4937049
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2025-08-13 14:08:36 +0300

    posting and attachment APIs cpmpleted

--- backend/main.py
+++ backend/main.py
@@ -11,17 +11,6 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/api/tables', methods=['GET'])
-def get_tables():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Account;")
-    tables = cursor.fetchall()
-    table_names = [table['User_Name'] for table in tables]
-    
-    conn.close()
-    
-    return jsonify(table_names)
 
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
@@ -67,14 +56,14 @@ def login():
     else:
         return jsonify({"Error": "Invalid email or password"}), 401
 
-@app.route('/uploadPost', methods=['POST'])
+@app.route('/uploadUserPost', methods=['POST'])
 def uploadPost():
     data = request.get_json()
     
     title = data.get('title')
     description = data.get('description')
-    user_id = data.get('user_id')
-    attachments = data.get('attachments', [])
+    user_id = data.get('U_id')
+    attachments = data.get('Attachments', [])
     
     if not title or not user_id:
         return jsonify({'error': 'Title and user_id are required'}), 400
@@ -82,9 +71,29 @@ def uploadPost():
     conn = get_db_connection()
     
     cursor = conn.cursor()
-    cursor.execute('')
     
-    return "Post Uploaded"
+    try:
+        cursor.execute('INSERT INTO Post (title, description, U_id, Post_time) VALUES (?, ?, ?, CURRENT_TIMESTAMP)',
+                (title, description, user_id))
+            
+        post_id = cursor.lastrowid
+            
+        for attachment in attachments:
+            a_type = attachment.get('A_type')
+            file_size = attachment.get('File_size')
+            a_name = attachment.get('A_name')
+            cursor.execute('INSERT INTO Attachments (A_type, File_size, A_name, A_time, P_Id) VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?)',
+                (a_type, file_size, a_name, post_id))
+            
+        conn.commit()
+        return jsonify({'Message': 'Post uploaded successfully'}), 201
+    except Exception as e:
+        return jsonify({'Error': str(e)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    
+    
 
 
 if __name__ == '__main__':
